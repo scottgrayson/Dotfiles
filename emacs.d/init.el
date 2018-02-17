98f466e94051632b8b3183a9d95031b27134cfb3
Scott Grayson <dscottgrayson@gmail.com>
2018-02-17 18:22:56 -0500

    added language packages, magit, and projectile

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,18 +15,14 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
  '(package-selected-packages
    (quote
-    (evil-indent-textobject evil-surround evil-leader counsel swiper rainbow-mode avy org-bullets beacon which-key use-package))))
+    (web-mode php-mode projectile git-gutter magit linum-relative linumm-relative symon diminish spaceline company dashboard rainbow-delimiters rainbow-delimeters sudo-edit hungry-delete zenburn-theme which-key use-package solarized-theme rainbow-mode org-bullets leuven-theme gruvbox-theme evil-surround evil-leader evil-indent-textobject counsel beacon avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
