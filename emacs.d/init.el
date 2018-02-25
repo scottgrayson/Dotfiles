e0b8f710913c474cf271c734eeaf65f347267cbe
Scott Grayson <dscottgrayson@gmail.com>
2018-02-25 17:58:51 -0500

    eshell history with C-n C-p. Kaolin dark theme hl visibility

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,3 +15,27 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("107693012b59b3a94faa869756333b8fe7224670f762ce97eb1dda89f03f5bcd" default)))
+ '(fci-rule-color "#f8fce8")
+ '(hl-paren-background-colors (quote ("#e8fce8" "#c1e7f8" "#f8e8e8")))
+ '(hl-paren-colors (quote ("#40883f" "#0287c8" "#b85c57")))
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets which-key web-mode use-package spaceline smartparens rainbow-mode rainbow-delimiters projectile plan9-theme phpunit org-bullets magithub kaolin-themes js2-mode hydra git-gutter general flycheck exec-path-from-shell evil-surround evil-magit evil-indent-textobject evil-collection emmet-mode diminish counsel company-php beacon ace-window ac-php)))
+ '(sml/active-background-color "#98ece8")
+ '(sml/active-foreground-color "#424242")
+ '(sml/inactive-background-color "#4fa8a8")
+ '(sml/inactive-foreground-color "#424242"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
