a8a7c6e2c0f4e42e41e30cbecc9c4f1d313c6302
Scott Grayson <dscottgrayson@gmail.com>
2020-05-18 11:24:07 -0400

    init.el update theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -35,6 +35,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0eb3c0868ff890b0c4ee138069ce2a8936a8a69ba150efa6bfb9fb7c05af5ec3" default)))
  '(evil-search-module (quote evil-search))
  '(org-agenda-files (quote ("~/hunting_seasons.org" "~/org/gcal.org")))
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
